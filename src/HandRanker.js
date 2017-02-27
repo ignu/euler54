33c14990bc9f9cc376e6ac1e7681ed46b0473128
Len Smith <len@barrison.com>
2017-02-26 17:23:32 -0800

    add straightChecker

--- src/HandRanker.js
+++ src/HandRanker.js
@@ -1,38 +1,69 @@
 import R from 'ramda'
 
-const handRanks = ['highCard', 'royalFlush']
+const handRanks = ['highCard', 'pair', 'twoPair', 'threeOfAKind', 'straight', 'flush', 'fullHouse', 'fourOfAKind', 'straightFlush', 'royalFlush']
+
+const largetCard = (cards) => {
+  let maxCard = {order: 0}
+
+  const updateLargest = (card) => {
+    if(card.order > maxCard.order) maxCard = card
+  }
+
+  R.forEach(updateLargest)(cards)
+
+  return maxCard
+}
+
+const isFlush = (hand) => {
+  return R.uniq(R.pluck('suit')(hand)).length == 1
+}
+
+const isStraight = (hand) => {
+  if(R.uniq(R.pluck('order', hand)).length < 5) return false
+
+  const orderedHand = R.reverse(R.sortBy(R.prop('order'))(hand))
+  return orderedHand[0].order - orderedHand[4].order == 4
+}
 
 const royalFlushRanker = {
-  rankOrder: 10,
-  check: () => null
+  check: (hand) => {
+    return isFlush(hand) && largetCard(hand).order == "14"
+  }
+}
+
+const getRankOrder = (type, hand) => {
+  return handRanks.indexOf(type) * 100 + largetCard(hand).order
+}
+
+const straightRanker = {
+  check: (hand) => {
+    if(!isStraight(hand)) return undefined
+
+    return {
+      type: 'straight',
+      rankOrder: getRankOrder('straight', hand)
+    }
+  }
 }
 
 const highCardRanker = {
-  rankOrder: 1,
-
   check: (hand) => {
-    let maxCard = {order: 0}
-
-    const updateLargest = (card) => {
-      if(card.order > maxCard.order) maxCard = card
-    }
-
-    R.forEach(updateLargest)(hand)
-
     return {
       type: 'highCard',
-      rankOrder: handRanks.indexOf('highCard'),
-      value: [maxCard]
+      rankOrder: getRankOrder('highCard', hand)
     }
   }
 }
 
 const handCheckers = [
   royalFlushRanker,
+  straightRanker,
   highCardRanker
 ]
 
 export default {
   handCheckers,
-  highCard : highCardRanker.check
+  highCard : highCardRanker.check,
+  straight: straightRanker.check,
+  royalFlush: royalFlushRanker.check
 }
--- src/HandRanker.test.js
+++ src/HandRanker.test.js
@@ -1,11 +1,59 @@
 import ranker from './HandRanker'
 import parser from './Parser'
 
-it('can find a high card', () => {
-  const line = '8C TS KC 9H 4S'
-  const hand = parser.parseLine(line)[0]
+describe("high card", () => {
+  it('can find a high card', () => {
+    const line = '8C TS KC 9H 4S'
+    const hand = parser.parseLine(line)[0]
 
-  const rank = ranker.highCard(hand).value[0]
+    const rank = ranker.highCard(hand)
 
-  expect(rank.order).toEqual(13)
+    expect(rank.rankOrder).toEqual(13)
+  })
+})
+
+
+describe("straightRanker", () => {
+  it("returns rank when a straight", () => {
+    const line = '4C 2S 3C 5H 6S'
+
+    const hand = parser.parseLine(line)[0]
+
+    const rank = ranker.straight(hand)
+
+    expect(rank).toBeTruthy()
+    expect(rank.rankOrder).toEqual(406)
+  });
+
+
+  it("returns falsy when not a straight", () => {
+    const line = '8C 8S 7C 6H 5S'
+    const hand = parser.parseLine(line)[0]
+
+    const rank = ranker.royalFlush(hand)
+
+    expect(rank).toBeFalsy()
+  });
+})
+
+//describe("flushRanker")
+
+describe("royalFlushRanker", () => {
+  xit("returns rank when there's a royal flush", () => {
+    const line = '8C TS KC 9H 4S'
+    const hand = parser.parseLine(line)[0]
+
+    const rank = ranker.royalFlush(hand)
+
+    expect(rank).toBeTruthy()
+  })
+
+  it("is falsey when not a royal flush", () => {
+    const line = '8C TS KC 9H 4S'
+    const hand = parser.parseLine(line)[0]
+
+    const rank = ranker.royalFlush(hand)
+
+    expect(rank).toBeFalsy()
+  })
 })
